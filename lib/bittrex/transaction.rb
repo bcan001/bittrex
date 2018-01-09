96a8525f25bf4f8a8f7807b5872125fb341974e7
bcaneba <bcaneba@gmail.com>
2018-01-08 19:39:46 -0600

    trying hardcode

--- lib/bittrex/transaction.rb
+++ lib/bittrex/transaction.rb
@@ -5,36 +5,23 @@ module Bittrex
     attr_reader :market, :quantity, :rate, :raw
 
     def initialize(attrs = {})
-      # @uuid = attrs['uuid']
-      # @id = attrs['Uuid'].to_s
       @market = attrs['Market']
       @quantity = attrs['Quantity']
       @rate = attrs['Rate']
       @raw = attrs
     end
 
-    # https://github.com/ericsomdahl/python-bittrex/issues/35
-    def buy
-      client.get('market/buylimit')
+    def self.buy(market,quantity,rate)
+      client.get('market/buylimit',{
+        market: market,
+        quantity: quantity,
+        rate: rate,
+      })
     end
-
-    # def self.sell(market,quantity,rate)
-    #   # client.get('market/selllimit').map{|data| new(data) }
-    #   client.get('market/selllimit', {market: market, quantity: quantity, rate: rate})
-    # end
-
-    # def self.cancel_order(uuid)
-    #   client.get('market/cancel', {uuid: uuid})
-    # end
-
-    # def self.get_order(uuid)
-    #   client.get('account/getorder', {uuid: uuid})
-    # end
-
 
     private
 
-    def client
+    def self.client
       @client ||= Bittrex.client
     end
   end
