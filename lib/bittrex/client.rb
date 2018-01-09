1936368d4a9002c54b4f46a59f9e562d5841a651
bcaneba <bcaneba@gmail.com>
2018-01-08 19:01:20 -0600

    trying hardcode

--- lib/bittrex/client.rb
+++ lib/bittrex/client.rb
@@ -3,16 +3,6 @@ require 'base64'
 require 'json'
 
 module Bittrex
-  # class DoNotEncoder
-  #   def self.encode(params)
-  #     buffer = ''
-  #     params.each do |key, value|
-  #       buffer << "#{key}=#{value}&"
-  #     end
-  #   return buffer.chop
-  #   end
-  # end
-
   class Client
     HOST = 'https://bittrex.com/api/v1.1'
     # https://bittrex.com/api/v2.0/key/balance/getbalances
@@ -28,13 +18,7 @@ module Bittrex
       nonce = Time.now.to_i
       response = connection.get do |req|
         url = "#{HOST}/#{path}"
-        # req.params.merge!(params)
-
-        # if url == 'https://bittrex.com/api/v1.1/market/buylimit'
-          req.url("https://bittrex.com/api/v1.1/market/buylimit?market=BTC-LTC&quantity=0.03&rate=0.01692523&apikey=#{key}&nonce=#{nonce}")
-        # else
-        #   req.url(url)
-        # end
+        req.url(url)
 
         puts url
         puts req
@@ -47,7 +31,6 @@ module Bittrex
 
         puts req
 
-
       end
 
       puts response.body
@@ -55,65 +38,6 @@ module Bittrex
       JSON.parse(response.body)['result']
     end
 
-    # def post(path, params = {}, headers = {})
-    #   nonce = Time.now.to_i
-    #   response = connection.post do |req|
-    #     url = "#{HOST}/#{path}"
-    #     req.params.merge!(params)
-    #     req.url(url)
-
-    #     if key
-    #       req.params[:apikey]   = key
-    #       req.params[:nonce]    = nonce
-    #       req.headers[:apisign] = signature(url, nonce)
-    #     end
-
-    #     puts req
-    #   end
-
-    #   puts response.body
-
-    #   JSON.parse(response.body)['result']
-    # end
-
-    # def post(path, params = {}, headers = {})
-    #   nonce = Time.now.to_i
-    #   response = connection.post do |req|
-    #     url = "#{HOST}/#{path}"
-    #     req.params.merge!(params)
-    #     req.url(url)
-
-    #     new_url = url + '?'
-
-    #     if key
-    #       # url = url.m
-    #       params.each do |key, val|
-    #         new_url += key.to_s
-    #         new_url += '='
-    #         new_url += val.to_s
-    #         new_url += '&' 
-    #       end
-
-    #       new_url = new_url[0..-2]
-
-    #       new_url += "&apikey=#{key}&nonce=#{nonce}"
-
-    #       req.params[:apikey]   = key
-    #       req.params[:nonce]    = nonce
-    #       # req.headers[:apisign] = signature(new_url, nonce)
-    #       req.headers[:apisign] = signature_post(new_url, nonce)
-    #     end
-
-    #     puts new_url
-    #     # puts url
-    #     puts req
-
-    #   end
-
-    #   puts response.body
-
-    #   # JSON.parse(response.body)['result']
-    # end
 
     private
 
@@ -122,21 +46,13 @@ module Bittrex
     # end
 
     def signature(url, nonce)
-      # OpenSSL::HMAC.hexdigest('sha512', secret, url)
-      # if url == 'https://bittrex.com/api/v1.1/market/buylimit'
-        sig = OpenSSL::HMAC.hexdigest('sha512', secret, "#{url}?market=BTC-LTC&quantity=0.03&rate=0.01692523&apikey=#{key}&nonce=#{nonce}")
-        # market: 'BTC-LTC',quantity: 0.03,rate: 0.01692523
-      # else
-      #   sig = OpenSSL::HMAC.hexdigest('sha512', secret, "#{url}?apikey=#{key}&nonce=#{nonce}")
-      # end
-      # sig
+      OpenSSL::HMAC.hexdigest('sha512', secret, "#{url}?apikey=#{key}&nonce=#{nonce}")
     end
 
     def connection
       @connection ||= Faraday.new(:url => HOST) do |faraday|
         faraday.request  :url_encoded
         faraday.adapter  Faraday.default_adapter
-        # faraday.options.params_encoder = DoNotEncoder
       end
     end
   end
